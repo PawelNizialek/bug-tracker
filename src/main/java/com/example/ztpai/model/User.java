560a7786996bb369ddc3f4c991c23f0a2825ec7e
pawel <pawel850m@gmail.com>
2021-03-05 08:43:23 +0100

    add validators

--- src/main/java/com/example/ztpai/model/User.java
+++ src/main/java/com/example/ztpai/model/User.java
@@ -1,15 +1,32 @@
 package com.example.ztpai.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
 
 @Table
 @Entity(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    @NotBlank(message = "Name cannot be empty!")
     private String firstName;
+    @NotBlank(message = "Last name cannot be empty!")
     private String lastName;
+    @Email
+    @NotEmpty
     private String email;
+    private Boolean enabled;
+    private Date created_at;
+    private String salt;
 }
