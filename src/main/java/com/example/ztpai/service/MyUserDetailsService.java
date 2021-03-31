42b385e141f962ac49573ad00e1af3ee86a5b6d3
pawel <pawel850m@gmail.com>
2021-03-31 23:39:18 +0200

    add jwt implementation

--- src/main/java/com/example/ztpai/service/MyUserDetailsService.java
+++ src/main/java/com/example/ztpai/service/MyUserDetailsService.java
@@ -2,23 +2,38 @@ package com.example.ztpai.service;
 
 import com.example.ztpai.model.User;
 import com.example.ztpai.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
         Optional<User> user = userRepository.findUserByEmail(email);
-        user.orElseThrow(()->new UsernameNotFoundException("not found: " + email));
-        return user.map(MyUserDetails::new).get();
+        if(user.isPresent()){
+            return new org.springframework.security.core.userdetails.User(
+                    user.get().getEmail(), user.get().getPassword(),user.get().getEnabled(),
+                    true, true, true,
+                    getAuthorities(user.get().getRole()));
+        }
+        throw new UsernameNotFoundException("not found: " + email);
     }
+    private Collection<? extends GrantedAuthority> getAuthorities(String authority){
+        return Collections.singletonList(new SimpleGrantedAuthority(authority));
+    }
+
 }
